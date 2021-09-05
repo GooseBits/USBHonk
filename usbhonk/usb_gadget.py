57546931f59ffa0ea476fe5073fbec926208a4a9
Sean LaPlante <laplante.sean@gmail.com>
2021-09-05 11:29:11 -0400

    A bunch more stuff

--- usbhonk/usb_gadget.py
+++ usbhonk/usb_gadget.py
@@ -1,21 +1,26 @@
+"""USB Gadget code."""
 from pathlib import Path
 
+
 class USBGadget:
-    """ USB Gadget Base Class """
+    """USB Gadget Base Class."""
+
     def __init__(self, name):
+        """Create a :class:`USBGadget`."""
         self.path = Path(f"/sys/kernel/config/usb_gadget/{name}")
         self.path.mkdir(parents=True)
 
     def __del__(self):
+        """Handle cleanup."""
         if self.is_active():
             self.deactivate()
         # TODO: Unlink
 
-    def is_active() -> bool:
-        pass
+    def is_active(self) -> bool:
+        """Check if it's active."""
 
-    def activate():
-        pass
+    def activate(self):
+        """Activate the USB gadget."""
 
-    def deactivate():
-        pass
+    def deactivate(self):
+        """Deactivate the USB gadget."""
